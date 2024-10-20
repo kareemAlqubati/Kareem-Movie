a4a7d1b825a73754fbecdf2d33af1ea5d8d80249
Kareem Alqbuty <salamkareem792@gmail.com>
2024-10-20 11:38:09 +0300

    change the provider into into server side HOC

--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -40,8 +40,7 @@ const Login = () => {
   };
 
   return (
-    <div 
-      className="relative flex items-center justify-center min-h-screen bg-cover bg-center" >
+    <div className="relative flex items-center justify-center min-h-screen bg-cover bg-center">
       <div className="absolute top-0 left-0 w-64 h-64 bg-blue-200 rounded-full transform -translate-x-1/2 -translate-y-1/2 opacity-30"></div>
       <div className="absolute bottom-0 right-0 w-96 h-96 bg-green-200 rounded-full transform translate-x-1/2 translate-y-1/2 opacity-30"></div>
       <div className="absolute top-0 right-0 w-32 h-32 bg-yellow-200 rounded-full transform translate-x-1/2 -translate-y-1/2 opacity-20"></div>
@@ -73,8 +72,7 @@ const Login = () => {
             Login
           </button>
         </form>
-        <p className="mt-4 text-center">
-          Don't have an account?{' '}
+        <p className="mt-4 text-center">Don't have an account?{' '}
           <Link href="/Register" className="text-blue-500">
             Register
           </Link>
